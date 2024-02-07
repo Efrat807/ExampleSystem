d0f9f19c034a59e56d8c5b1935b58c78c40bcb47
efrat <efrat.shefer@agamimp.net>
2024-02-07 11:30:39 +0200

    useReducer example

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,5 +1,32 @@
+import { useReducer } from 'react';
+
+interface CounterState {
+	count: number;
+}
+
+type Action = { type: 'increment' } | { type: 'decrement' };
+
+const reducer = (state: CounterState, action: Action): CounterState => {
+	switch (action.type) {
+		case 'increment':
+			return { count: state.count + 1 };
+		case 'decrement':
+			return { count: state.count - 1 };
+		default:
+			throw new Error();
+	}
+};
+
 const Home = () => {
-	return <h1>this is HomePage</h1>;
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+	return (
+		<div>
+			<h1>Count: {state.count}</h1>
+			<button onClick={() => dispatch({ type: 'increment' })}>+</button>
+			<button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+		</div>
+	);
 };
 
 export default Home;
